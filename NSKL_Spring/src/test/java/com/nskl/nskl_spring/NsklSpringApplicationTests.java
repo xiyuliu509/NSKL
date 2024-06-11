279a0360494878d5eaf0637358108d953a4cb20e
honarly <honarly@163.com>
2024-06-11 14:34:34 +0800

    搭建实体类，尝试运用mybatis-plus来简化sql，连通数据库

---
+++ NSKL_Spring/src/test/java/com/nskl/nskl_spring/NsklSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nskl.nskl_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsklSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
