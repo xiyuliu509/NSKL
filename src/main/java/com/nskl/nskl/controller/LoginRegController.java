c7b16e35d5547d7246b779b2abd3fcbf29fa8487
gurunhuru <124226852+gurunhuru@users.noreply.github.com>
2024-06-24 10:28:43 +0800

    用户模块完成

---
+++ src/main/java/com/nskl/nskl/controller/LoginRegController.java
@@ -0,0 +1,48 @@
+package com.nskl.nskl.controller;
+
+import com.nskl.nskl.bean.RespBean;
+import com.nskl.nskl.bean.User;
+import com.nskl.nskl.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class LoginRegController
+{
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/login_error")
+    public RespBean loginError() {
+        return new RespBean("error", "登录失败!");
+    }
+
+    @RequestMapping("/login_success")
+    public RespBean loginSuccess() {
+        return new RespBean("success", "登录成功!");
+    }
+    
+    @RequestMapping("/login_page")
+    public RespBean loginPage() {
+        return new RespBean("error", "尚未登录，请登录!");
+    }
+
+    @RequestMapping("/reg")
+    public RespBean reg(User user) {
+        int result = userService.reg(user);
+        if (result == 0) {
+            //成功
+            return new RespBean("success", "注册成功!");
+        } else if (result == 1) {
+            return new RespBean("error", "用户名重复，注册失败!");
+        } else {
+            //失败
+            return new RespBean("error", "注册失败!");
+        }
+    }
+}
